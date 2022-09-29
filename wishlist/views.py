d4b42e63ce2c22e9a0fbea1896438cf4a5fb1f44
Salma Hanifah <salma.hanifah11@ui.ac.id>
2022-09-29 16:10:26 +0700

    LAB 4

--- wishlist/views.py
+++ wishlist/views.py
@@ -21,7 +21,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Salma',
+    'nama': 'Salma Hanifah Hidayanti',
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
